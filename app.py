733bcecc5596bc2ffc9c9a9c22ff345c0f34471d
razashiksha-commits <raza.shiksha@gmail.com>
2026-07-21 13:51:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="UILP Dashboard",
+    layout="wide"
+)
+
+st.title("📊 UILP SEO Dashboard")
+st.success("Dashboard is running successfully!")
